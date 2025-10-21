0a17337c674b1aca08167ef13f228b4b4f63f758
huongniz <22070956@vnu.edu.vn>
2025-10-21 12:10:33 +0700

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+include "connection.php";
+
+$id=$_GET["id"];
+
+// get some information about the item to be deleted (optional)
+$res = mysqli_query($link,"SELECT * FROM table1 WHERE id=$id");
+$item = mysqli_fetch_array($res);
+
+if(isset($_GET["confirm"]) && $_GET["confirm"]=="yes"){
+    $delete_query = "DELETE FROM table1 WHERE id=$id";
+    mysqli_query($link,$delete_query) or die(mysqli_error($link));
+    header("Location: index.php");
+}
+?>
+
+<!-- print confirm message to user -->
+<h1>Are you sure to delete item <?php echo $item["firstname"] . " " . $item["lastname"]; ?>?</h1>
+<button><a href="delete.php?id=<?php echo $id ?>&confirm=yes">Yes</a></button>
+<button><a href="index.php">No</a></button>
